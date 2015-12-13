eecbd0b8ca01a5fc40d42fd9a2897dd55f2e6926
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-13 15:55:47 -0800

    Complete day 1 part 2

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -23,10 +23,30 @@ fn get_final_floor(input: &str) -> i32 {
 	return current_floor;
 }
 
+fn get_first_basement_position(input: &str) -> Option<i32> {
+	let mut current_floor = 0;
+	let mut position = 1;
+	for c in input.chars() {
+		let delta = match c {
+			'(' => 1,
+			')' => -1,
+            _ => 0
+		};
+		current_floor += delta;
+		if(current_floor < 0) {
+			return Some(position);
+		}
+
+		position += 1;
+	}
+
+	return None;
+}
+
 fn main() {
 	println!("Advent of Code - day 1");
 
 	let input = read_file("input.txt").unwrap();
-	let final_floor = get_final_floor(&input);
-	println!("Answer: {}", final_floor);
+	println!("Part 1 answer: {}", get_final_floor(&input));
+	println!("Part 2 answer: {}", get_first_basement_position(&input).unwrap());
 }
