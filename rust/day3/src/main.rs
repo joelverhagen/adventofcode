4c625ddf75cb5f9b54cd40c2a66f4aca5f5464d8
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-13 16:38:41 -0800

    Complete day 3 part 2

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -17,19 +17,23 @@ fn increment_key(position: (i32, i32), matrix: &mut HashMap<(i32, i32), i32>) ->
 	}
 }
 
-fn build_matrix(input: &str) -> HashMap<(i32, i32), i32> {
+fn move_position(position: (i32, i32), c: char) -> Option<(i32, i32)> {
+	match c {
+		'^' => Some((position.0 - 1, position.1)),
+		'v' => Some((position.0 + 1, position.1)),
+		'>' => Some((position.0, position.1 + 1)),
+		'<' => Some((position.0, position.1 - 1)),
+		_ => None,
+	}
+}
+
+fn build_matrix_part_1(input: &str) -> HashMap<(i32, i32), i32> {
 	let mut matrix = HashMap::new();
 	let mut position = (0, 0);
 	increment_key(position, &mut matrix);
 
 	for c in input.chars() {
-		let new_position = match c {
-			'^' => Some((position.0 - 1, position.1)),
-			'v' => Some((position.0 + 1, position.1)),
-			'>' => Some((position.0, position.1 + 1)),
-			'<' => Some((position.0, position.1 - 1)),
-			_ => None,
-		};
+		let new_position = move_position(position, c);
 
 		if new_position.is_some() {
 			position = new_position.unwrap();
@@ -40,10 +44,43 @@ fn build_matrix(input: &str) -> HashMap<(i32, i32), i32> {
 	return matrix;
 }
 
+
+fn build_matrix_part_2(input: &str) -> HashMap<(i32, i32), i32> {
+	let mut matrix = HashMap::new();
+	let mut position_a = (0, 0);
+	let mut position_b = (0, 0);
+	increment_key(position_a, &mut matrix);
+	increment_key(position_b, &mut matrix);
+	let mut next_is_a = true;
+
+	for c in input.chars() {
+		let position = match next_is_a {
+			true => position_a,
+			false => position_b,
+		};
+
+		let new_position = move_position(position, c);
+
+		if new_position.is_some() {
+
+			if next_is_a {
+				position_a = new_position.unwrap();
+			} else {
+				position_b = new_position.unwrap();
+			}
+			
+			increment_key(new_position.unwrap(), &mut matrix);
+			next_is_a = !next_is_a;
+		}
+	}
+
+	return matrix;
+}
+
 fn main() {
     println!("Advent of Code - day 3");
 
 	let input = read_file("input.txt").unwrap();
-	let matrix = build_matrix(&input);
-	println!("Answer: {}", matrix.len());
+	println!("Part 1 answer: {}", build_matrix_part_1(&input).len());
+	println!("Part 2 answer: {}", build_matrix_part_2(&input).len());
 }
