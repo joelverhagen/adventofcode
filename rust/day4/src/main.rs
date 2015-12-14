0a93a78e209763c4fcd3cb320d96594e8d1fa904
Joel Verhagen <joel.verhagen@gmail.com>
2015-12-13 17:21:19 -0800

    Complete day 4 part 2

--- rust/day4/src/main.rs
+++ rust/day4/src/main.rs
@@ -1,26 +1,47 @@
-extern crate crypto;
+use std::thread;
+use std::sync::mpsc;
 
+extern crate crypto;
 use crypto::md5::Md5;
 use crypto::digest::Digest;
 
-fn find_answer(key: &str) -> i32 {
-	let mut current = 0;
-	let mut md5 = Md5::new();
+struct Input {
+	start: i32,
+	increment: i32,
+	key: String,
+	prefix: String,
+}
 
-	loop {
-		let hash_input = format!("{}{}", key, current);
-		md5.input_str(&hash_input);
-		let hash_output = md5.result_str();
-		md5.reset();
 
-		if hash_output.starts_with("00000") {
-			break;
-		} else {
-			current += 1;
-		}
+fn find_answer(key: &str, prefix: &str) -> i32 {
+	let increment = 8;
+	let mut children = Vec::new();
+	let (tx, rx) = mpsc::channel();
+	
+	for start in 0..increment {
+		let tx = tx.clone();
+		let input = Input { start: start, increment: increment, key: key.to_string(), prefix: prefix.to_string() };
+		let child = thread::spawn(move || {
+			let mut current = input.start;
+			let mut md5 = Md5::new();
+			loop {
+				let hash_input = format!("{}{}", input.key, current);
+				md5.input_str(&hash_input);
+				let hash_output = md5.result_str();
+				md5.reset();
+
+				if hash_output.starts_with(&input.prefix) {
+					tx.send(current);
+				} else {
+					current += input.increment;
+				}
+			}
+		});
+
+		children.push(child);
 	}
 
-	return current;
+	return rx.recv().unwrap();
 }
 
 
@@ -29,5 +50,6 @@ fn main() {
 
 	let input = "yzbqklnj";
     println!("Secret key: {}", input);
-    println!("Answer: {}", find_answer(input));
+    // println!("Part 1 answer: {}", find_answer(input, "00000"));
+    println!("Part 2 answer: {}", find_answer(input, "000000"));
 }
