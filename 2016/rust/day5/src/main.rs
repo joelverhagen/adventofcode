7a27f34a200b6a967892128d83043d04d8bef7ca
Joel Verhagen <joel.verhagen@gmail.com>
2016-12-17 13:10:37 -0800

    Complete day 5 part 2

--- 2016/rust/day5/src/main.rs
+++ 2016/rust/day5/src/main.rs
@@ -7,11 +7,11 @@ fn get_hex_char(b: u8) -> Option<char> {
     match b {
         0  ...  9 => Some((b + ('0' as u8)) as char),
         10 ... 15 => Some(((b - 10) + ('a' as u8)) as char),
-        _         => None
+        _         => None,
     }
 }
 
-fn get_password_char(hasher: &mut Md5, door_id_bytes: &[u8], index: u64) -> Option<char> {
+fn get_part_1_password_char(hasher: &mut Md5, door_id_bytes: &[u8], index: u64) -> Option<char> {
     hasher.input(&door_id_bytes);
     hasher.input(index.to_string().as_bytes());
 
@@ -25,13 +25,13 @@ fn get_password_char(hasher: &mut Md5, door_id_bytes: &[u8], index: u64) -> Opti
     }
 }
 
-fn get_password(door_id: &str) -> String {
+fn get_part_1_password(door_id: &str) -> String {
     let door_id_bytes = door_id.as_bytes();
     let mut password = String::new();
     let mut hasher = Md5::new();
 
     for index in 0.. {
-        match get_password_char(&mut hasher, &door_id_bytes, index) {
+        match get_part_1_password_char(&mut hasher, &door_id_bytes, index) {
             Some(c) => password.push(c),
             None    => continue,
         };
@@ -46,7 +46,52 @@ fn get_password(door_id: &str) -> String {
     password
 }
 
+fn get_part_2_password_char(hasher: &mut Md5, door_id_bytes: &[u8], index: u64) -> Option<(usize, char)> {
+    hasher.input(&door_id_bytes);
+    hasher.input(index.to_string().as_bytes());
+
+    let mut hash = [0; 16];
+    hasher.result(&mut hash);
+    hasher.reset();
+
+    match (hash[0], hash[1], hash[2], hash[3] / 16) {
+        (0, 0, p, b) if p < 8 && b < 16 => Some((p as usize, get_hex_char(b).unwrap())),
+        _                               => None,
+    }
+}
+
+fn get_part_2_password(door_id: &str) -> String {
+    let door_id_bytes = door_id.as_bytes();
+    let mut password: Vec<char> = vec![' '; 8];
+    let mut completed = 0;
+    let mut hasher = Md5::new();
+
+    for index in 0.. {
+        match get_part_2_password_char(&mut hasher, &door_id_bytes, index) {
+            Some((p, c)) if password[p] == ' ' => password[p] = c,
+            _                                  => continue,
+        };
+
+        // println!("{}{} -> {:?}", door_id, index, password);
+
+        let password_display: String = password.iter().cloned().collect();
+        println!("{}", password_display);
+        
+        completed += 1;
+        if completed >= password.len() {
+            break;
+        }
+    }
+
+    password.into_iter().collect()
+}
+
 fn main() {
-    let day_1_result = get_password("uqwqemis");
+    let door_id = "uqwqemis";
+
+    let day_1_result = get_part_1_password(door_id);
     println!("Part 1 result: {}", day_1_result);
+
+    let day_2_result = get_part_2_password(door_id);
+    println!("Part 2 result: {}", day_2_result);
 }
